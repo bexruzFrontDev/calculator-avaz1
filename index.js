cd9798211ce7c42f8fd824d03f3c5147ecd582c9
zafarbek-portfol <162569630+bexruzFrontDev@users.noreply.github.com>
2024-05-18 11:36:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let oyna = document.querySelector('.oyna')
+
+function Oynaga(input) {
+    oyna.value += input
+}
+
+function tozalash () {
+    oyna.value = ''
+}
+
+function calculator () {
+    try {
+        oyna.value = eval(oyna.value)
+    } catch (error) {
+        oyna.value = 'Error!'
+    }
+}
